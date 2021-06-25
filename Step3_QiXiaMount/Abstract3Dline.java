851cdb0c7d814ea2259a3c156fe208e47c4b6dbb
NNUwdl <nnuwxl@gmail.com>
2021-06-25 14:57:28 +0800

    Add files via upload

---
+++ Step3_QiXiaMount/Abstract3Dline.java
@@ -0,0 +1,14 @@
+package Step3_QiXiaMount;
+
+/**
+ * - coding: utf-8 -
+ * Time    : 2021/5/27 20:00
+ * Author  : Xilun Wu
+ * Email   : nnuwxl@gmail.com
+ * File    : Abstract3Dline.java
+ */
+
+public interface Abstract3Dline {
+    float fromx(float x);
+    float fromy(float y);
+}
